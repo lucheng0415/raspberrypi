7b0c9dccba4b4cf08050b3422d4ce4fa75965d35
Cheng Lu <jason.cheng.lu@hotmail.com>
2017-10-15 12:18:44 +1300

    update backup file for polycomp - (it's a rubish)
    Signed-off-by: Cheng Lu <jason.cheng.lu@hotmail.com>

--- switch_led.py
+++ switch_led.py
@@ -12,12 +12,11 @@ switch_mid = 35
 switch_add = 33
 switch_min = 37
 
-time_loop_init = 1.2
-time_step = 0.2
-time_max = 2
-time_min = 0.2
+time_loop_init = 0.1
+time_step = 0.01/2
+time_max = 1
+time_min = 0.05
 
-time_init = 1.2
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin_toggle, GPIO.OUT)
 GPIO.setup(switch_mid, GPIO.OUT)
@@ -26,51 +25,32 @@ GPIO.setup(switch_min, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 GPIO.output(35, GPIO.HIGH)
 
-def func_timer_add():
-    global time_loop_init
-    if GPIO.HIGH == GPIO.input(switch_add):
-        print(' --- add long press --- ')
-        time_loop_init = time_max
-	
-
-def func_timer_min():
-    global time_loop_init
-    if GPIO.HIGH == GPIO.input(switch_min):
-        print(' --- min long press --- ')
-        time_loop_init = time_min
-
 def switch_add_cb(switch_add):
     global time_loop_init
     if time_loop_init < time_max:
-        print (' --- add short press --- ')
+        print ('add pressed')
         time_loop_init += time_step
-        timer = threading.Timer(1, func_timer_add)
-        timer.start()
     else:
-        print ('reached max key press count')
+        print ('reached add key limitation')
   
-def switch_min_cb(switch_min):
+def switch_reduce_cb(switch_min):
     global time_loop_init
     if time_loop_init > time_min:
-        print (' --- min short press --- ')
+        print ('reduce pressed')
         time_loop_init -= time_step
-        timer = threading.Timer(1, func_timer_min)
-        timer.start()
     else:
-        print ('reached min  key press count')
+        print ('reached reduce key limitation')
 
-GPIO.add_event_detect(switch_add, GPIO.RISING, callback = switch_add_cb, bouncetime = 300)
-GPIO.add_event_detect(switch_min, GPIO.RISING, callback = switch_min_cb, bouncetime = 300)
+GPIO.add_event_detect(switch_add, GPIO.RISING, callback = switch_add_cb, bouncetime = 1000)
+GPIO.add_event_detect(switch_min, GPIO.RISING, callback = switch_reduce_cb, bouncetime = 1000)
 
 print('Board and callbacks init finished!')
 
 while True:
     GPIO.output(pin_toggle, GPIO.HIGH)
-    time.sleep(time_loop_init)
+    ftime.sleep(time_loop_init)
     GPIO.output(pin_toggle, GPIO.LOW)
-    time.sleep(time_loop_init)
-
+    ftime.sleep(time_loop_init)
 print ('while loop started !')
-
 #No need cleanup just keep it here for mindset
 GPIO.clearnup()
